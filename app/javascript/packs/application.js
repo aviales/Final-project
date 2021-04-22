2af6c0f08b32c7d11fb20784ff1e96ce46bae677
alonso <avialesarrieta@gmail.com>
2021-04-22 12:20:06 -0600

    model adminuser changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require rails-ujs
+//= require chartkick
+//= require highcharts
+//= require activestorage
+//= require jquery/dist/jquery
+//= require turbolinks
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,5 @@
 class AdminUser < ApplicationRecord
 
-  has_many :contractors
-  has_many :projects
-  has_many :inspeccions
-  has_many :check_lists
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
