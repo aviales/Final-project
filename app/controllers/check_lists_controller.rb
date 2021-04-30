a455a62fe3906bd62189b0cb64b8bbaa32e3c32d
alonso <avialesarrieta@gmail.com>
2021-04-29 22:04:47 -0600

    css changes

--- app/controllers/check_lists_controller.rb
+++ app/controllers/check_lists_controller.rb
@@ -63,7 +63,7 @@ class CheckListsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_check_list
-      @check_list = CheckList.includes(params[:inspeccio_id])
+      @check_list = CheckList.includes(params[:inspeccion_id])
       
     end
     def set_inspeccion
