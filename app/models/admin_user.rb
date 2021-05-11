433628b65ba82e5e812ea5ac1beeb2db95b05f83
alonso <avialesarrieta@gmail.com>
2021-05-11 16:49:51 -0600

    repair

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,10 @@
 class AdminUser < ApplicationRecord
+
+  has_many :check_list
+  has_many :projects
+  has_many :contractors
+  has_many :inspeccions
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,12 @@
 class Project < ApplicationRecord
+<<<<<<< HEAD
  
   before_save do
     self.periodicity.gsub!(/[\[\]\"]/, "") if attribute_present?("periodicity")
   end
 
+=======
+>>>>>>> activeadmin models
   has_many :inspeccions, dependent: :destroy
    belongs_to :user, :optional => true
 
