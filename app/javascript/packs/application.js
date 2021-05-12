5000739a316250d113ed5c11f1c409f1c0f3877d
Steyler <steylerap@gmail.com>
2021-05-11 23:58:24 -0600

    changes for heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,9 +16,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import Chartkick from 'chartkick'
-import Highcharts from 'highcharts'
-window.Highcharts = Highcharts
+
 
 //import  "../layouts/application.css"
 
